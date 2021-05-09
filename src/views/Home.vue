cf9f29bcb6b037868dd0f31c0d99c00a5a71e67f
Dendi <352671309@qq.com>
2021-05-09 18:09:24 +0800

    feat: 完成tab组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <m-header />
+    <tab />
   </div>
 </template>
 
 <script>
 import Header from 'components/Header/';
+import Tab from 'components/Tab/';
 
 export default {
   name: 'home',
   components: {
     'm-header': Header,
+    Tab,
   },
 };
 </script>
